b7d86038f0b6f4103eb2264f8fe6f9c1ff76535a
JTrevena <jacobtrevena@gmail.com>
2023-01-06 11:10:06 +0000

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const serverless = require('serverless-http');
-const express = require('express')
-const test = require('./routes/test')
+const express = require('express');
+const test = require('./routes/test');
 
 
-const app = express()
+const app = express();
 
 app.get('/hello', function(req,res){
   res.send('Hello from specific path endpoint.')
 })
-app.use('/test',test)
+app.use('/test',test);
 
 module.exports.lambdaHandler = serverless(app, {basePath:'/api'});
